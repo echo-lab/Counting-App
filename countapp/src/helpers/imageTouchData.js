15eacd9e4d4655fa80d0b73f0d3ae9e16e358c5f
Sulakna Karunaratna <sulakna.karunaratna@arimaclanka.com>
2024-01-15 12:26:10 -0500

    Adding touch data to database

--- countapp/src/helpers/imageTouchData.js
+++ countapp/src/helpers/imageTouchData.js
@@ -1,6 +1,12 @@
 import { BASE_URL } from '../helpers/constants.js';
 
+import { decode } from 'jsonwebtoken';
+
 export const handleInteraction = (event, setTouchData) => {
+
+  const token = window.localStorage.getItem("token");
+  const decodedToken = decode(token);
+  const userName = decodedToken.name;
   const date = new Date().toLocaleDateString();
   const currentTime = new Date();
   const milliseconds = currentTime.getMilliseconds();
@@ -10,6 +16,7 @@ export const handleInteraction = (event, setTouchData) => {
   const touchY = event.clientY || (event.touches && event.touches[0].clientY);
 
   const interaction = {
+    userName: userName,
     x: touchX,
     y: touchY,
     date: date,
@@ -31,38 +38,22 @@ export const handleInteraction = (event, setTouchData) => {
   };
 
 
-  export const handleNextClickBaseline = (touchData) => {
-    console.log("touchdata:", touchData)
-    fetch(`${BASE_URL}/save/Game/Touch/Baseline/Data`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ touchData }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Touch data sent successfully.');
-        } else {
-          console.error('Failed to send touch data to the backend.');
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-
-  };
 
   
 
-  export const handleNextClickTouch = (touchData) => {
+
+  export const handleNextClickTouchData = (touchData,category, pageNo) => {
     console.log("touchdata:", touchData)
-    fetch(`${BASE_URL}/save/Game/Touch/Data`, {
+    fetch(`${BASE_URL}/save/Touch/Data`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ touchData }),
+      body: JSON.stringify({ 
+        touchData: touchData,
+        category: category,
+        pageNumber : pageNo,
+      }),
     })
       .then((response) => {
         if (response.ok) {
@@ -77,14 +68,18 @@ export const handleInteraction = (event, setTouchData) => {
 
   };
 
-  export const handleNextClickAnimation = (touchData) => {
+  export const handleNextClickTraining = (touchData, typeOfTraining, pageNo) => {
     console.log("touchdata:", touchData)
-    fetch(`${BASE_URL}/save/Game/Touch/Animation/Data`, {
+    fetch(`${BASE_URL}/save/Training/TouchData`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ touchData }),
+      body: JSON.stringify({
+        touchData: touchData,
+        category: typeOfTraining,
+        pageNumber : pageNo,
+      }),
     })
       .then((response) => {
         if (response.ok) {
@@ -99,36 +94,18 @@ export const handleInteraction = (event, setTouchData) => {
 
   };
 
-  export const handleNextClickTraining = (touchData, typeOfTraining) => {
+  export const handleNextClickPractice = (touchData, pageNo) => {
     console.log("touchdata:", touchData)
-    fetch(`${BASE_URL}/saveTrainingTouchData`, {
+    fetch(`${BASE_URL}/save/Practice/TouchData`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ touchData, typeOfTraining }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Touch data sent successfully.');
-        } else {
-          console.error('Failed to send touch data to the backend.');
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-
-  };
-
-  export const handleNextClickPractice = (touchData) => {
-    console.log("touchdata:", touchData)
-    fetch(`${BASE_URL}/savePracticeTouchData`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ touchData }),
+      body: JSON.stringify({
+        touchData: touchData,
+        category: "Practice",
+        pageNumber : pageNo,
+      }),
     })
       .then((response) => {
         if (response.ok) {
